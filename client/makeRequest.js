0ea50c7a6bd286938eafc7bc5aaa2862b397ce32
AayushG02 <aayush.girdhar2@gmail.com>
2023-11-30 22:32:44 +0530

    added intercept to get token before each req

--- client/makeRequest.js
+++ client/makeRequest.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 
-const token = JSON.parse(localStorage.getItem("user"))?.user?.token;
+// Create a new Axios instance
 export const makeRequest = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization: `Bearer ${token ? token : ""}`,
-  },
 });
+
+// Add an interceptor to update the authorization header before each request
+makeRequest.interceptors.request.use(
+  (config) => {
+    // Get the latest token from local storage
+    const userToken = JSON.parse(localStorage.getItem("user"))?.user?.token;
+
+    // Update the authorization header
+    config.headers.Authorization = `Bearer ${userToken || ""}`;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
