42f92bfcb1c68f34db13e8db38a4f5565dbac524
AayushG02 <aayush.girdhar2@gmail.com>
2024-01-19 22:35:14 +0530

    added google auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_AUTH_CLIENT_ID}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </GoogleOAuthProvider>
 );
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,15 @@
 import { useState, useRef } from "react";
 import { useLogin } from "../../hooks/useLogin";
 import { useSignup } from "../../hooks/useSignup";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/userReducer";
+import { jwtDecode } from "jwt-decode";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 import "./Login.css";
 import image from "../../assets/login.svg";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 const Login = () => {
   const formRef = useRef(null);
   const { login, loading, error } = useLogin();
@@ -12,6 +17,8 @@ const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const location =
     useLocation().pathname.split("/")[2].charAt(0).toUpperCase() +
@@ -23,7 +30,27 @@ const Login = () => {
       ? await login(email, password)
       : await signup(name, email, password);
   };
-
+  const handleGoogleLogin = async (googleUser) => {
+    const user = jwtDecode(googleUser.credential);
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/user/googleLogin`,
+        { email: user.email, name: user.name },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.status === 200) {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        dispatch(setUser(res.data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="login-wrapper">
       <div className="login-container">
@@ -33,7 +60,7 @@ const Login = () => {
         <div className="login-right">
           <div className="form-container">
             <Link className="link" to="/">
-              <h1 className="login-header">LuxeThreads</h1>
+              <h1 className="login-header">{location}</h1>
             </Link>
             <form ref={formRef} onSubmit={handleSubmit}>
               {location === "Signup" && (
@@ -94,63 +121,20 @@ const Login = () => {
                 {location === "Login" ? "Sign up" : "Login"}
               </Link>
             </p>
+            <div className="container">
+              <hr className="hr-text" data-content="or better continue with" />
+            </div>
+            <div className="google-login">
+              <GoogleLogin
+                clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
+                onSuccess={(res) => handleGoogleLogin(res)}
+                onError={(err) => console.log(err)}
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    // <div className="login-container">
-    //   <div className="login-left">
-    //     <img src={image} alt="Register Image" />
-    //   </div>
-    //   <div className="login-right">
-    //     <h1 className="login-header">Log In</h1>
-    //     <form onSubmit={handleLogin}>
-    //       <div className="floating-label-group">
-    //         <input
-    //           type="text"
-    //           className="login-name"
-    //           required
-    //           italize="true"
-    //           onChange={(e) => setUsername(e.target.value)}
-    //         />
-    //         <span className="floating-label">Username</span>
-    //       </div>
-
-    //       <div className="floating-label-group">
-    //         <input
-    //           type="password"
-    //           className="login-email"
-    //           required
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         />
-    //         <span className="floating-label">Password</span>
-    //       </div>
-
-    //       <span className="forgot">Forgot password?</span>
-    //       <button className="login-submit">
-    //         {loading ? "Logging in..." : "Login"}
-    //       </button>
-    //     </form>
-    //     <div className="container">
-    //       <hr className="hr-text" data-content="or better continue with" />
-    //     </div>
-    // {/* <div className="login-btn-container">
-    //   <div className="login-google">
-    //     <GoogleIcon /> Google
-    //   </div>
-    //   <div className="login-google">
-    //     <AppleIcon /> Apple
-    //   </div>
-    //   <div className="login-google">
-    //     <LinkedInIcon /> LinkedIn
-    //   </div>
-    // </div> */}
-    // {/* <span className="no-account">
-    //   Don&apos;t have an account? <Link className="link">Sign up</Link>
-    // </span> */}
-    //   </div>
-    // </div>
   );
 };
 
