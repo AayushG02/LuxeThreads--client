168e0192bb4971e7fbec1b21bdc5b4d83e9f6f8f
AayushG02 <aayush.girdhar2@gmail.com>
2024-03-05 00:39:25 +0530

    minor changes

--- client/src/components/Skeleton/SkeletonProductPage.jsx
+++ client/src/components/Skeleton/SkeletonProductPage.jsx
@@ -4,23 +4,23 @@ const SkeletonProductPage = () => {
   return (
     <div className="skeleton-product-container">
       <div className="skeleton-product-left">
-          <div className="skeleton-preview-img">
-            <div className="skeleton-img-product"></div>
-            <div className="skeleton-img-product"></div>
-          </div>
-          <div className="skeleton-selected-img">
-            <div className="skeleton-img-product"></div>
-          </div>
+        <div className="skeleton-preview-img">
+          <div className="skeleton-img-product"></div>
+          <div className="skeleton-img-product"></div>
         </div>
-        <div className="skeleton-product-right">
-          <div className="skeleton-product-title"></div>
-          <div className="skeleton-product-price"></div>
-          <div className="skeleton-product-desc"></div>
-          <div className="skeleton-product-quantity"></div>
-          <div className="skeleton-quantity-btns"></div>
-          <div className="skeleton-add-cart"></div>
+        <div className="skeleton-selected-img">
+          <div className="skeleton-img-product"></div>
         </div>
       </div>
+      <div className="skeleton-product-right">
+        <div className="skeleton-product-title"></div>
+        <div className="skeleton-product-price"></div>
+        <div className="skeleton-product-desc"></div>
+        <div className="skeleton-product-quantity"></div>
+        <div className="skeleton-quantity-btns"></div>
+        <div className="skeleton-add-cart"></div>
+      </div>
+    </div>
   );
 };
 
